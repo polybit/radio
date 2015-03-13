253f268643e338893921b2a193c306b288d76e51
Aaron Kirkbride <aaron@aaronkirkbride.com>
2015-03-13 21:18:41 +0000

    Add config

---
+++ config.py
@@ -0,0 +1,16 @@
+class Config(object):
+    DEBUG = False
+    TESTING = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
