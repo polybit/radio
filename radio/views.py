405049f62bf09e154c147ffb87069469d3e30be1
Aaron Kirkbride <aaron@aaronkirkbride.com>
2015-03-13 21:41:55 +0000

    Render index.html template

--- radio/views.py
+++ radio/views.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from flask import render_template
+
 from radio import app
 
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template('index.html')
