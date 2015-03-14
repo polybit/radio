49dbc5e4f6dc33ac2db4728d270418d32b680c95
Joe Bateson <joe@joebateson.com>
2015-03-14 19:36:23 +0000

    Move frontend code to /src

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,25 @@ var gulp = require('gulp');
 var react = require('gulp-react');
 var watch = require('gulp-watch');
 
-var staticDir = 'radio/static';
+var srcDir = 'src/';
+var dstDir = 'radio/static/';
 
 gulp.task('compile-jsx', function() {
-  return gulp.src(staticDir + '/jsx/**/*.jsx')
+  return gulp.src(srcDir + '**/*.jsx')
         .pipe(react())
         .on('error', function(e) {
             console.error(e.message + '\n  in ' + e.fileName);
         })
-        .pipe(gulp.dest(staticDir + '/js/modules/'));
+        .pipe(gulp.dest(dstDir));
 });
 
 gulp.task('watch', function() {
-      gulp.watch(staticDir + '/jsx/**/*.jsx', ['compile-jsx']);
+      gulp.watch(srcDir + '**/*.jsx', ['compile-jsx']);
+});
+
+// Move everything non-jsx from src -> static
+gulp.task('default', ['compile-jsx'], function() {
+    return gulp.src([srcDir + '**/*.html', srcDir + '**/*.js'])
+          .pipe(gulp.dest(dstDir));
 });
 
--- radio/views.py
+++ radio/views.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from flask import render_template
-
 from radio import app
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return app.send_static_file('index.html')
